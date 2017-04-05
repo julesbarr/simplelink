be94e00243ed9cb6174ef2a2dde0db71feefad73
Franco Julián Barrionuevo <barriojules@gmail.com>
2017-04-05 15:48:48 -0300

    added missing chalk

--- libs/configHandler.js
+++ libs/configHandler.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const chalk = require('chalk');
 const moduleInterface = {
   write,
   read
@@ -8,7 +9,7 @@ const moduleInterface = {
 function write(config) {
   fs.writeFile(path.resolve(__dirname, 'config.json'), JSON.stringify(config), (err) => {
     if (!err) {
-      console.log( chalk.green(`Path changed to: ${program.path}`) );
+      console.log( chalk.green(`Path changed to: ${config.path}`) );
     }
     process.exit(0);
   });
@@ -17,7 +18,8 @@ function write(config) {
 function read(init) {
   fs.readFile(path.resolve(__dirname, 'config.json'), (err, data) => {
     if (err) {
-      onError('Can\'t open config file, you must specify a path before using autolink');
+      console.log( chalk.red('Can\'t open config file, you must specify a path before using autolink'));
+      process.exit(1);
     } else {
       init(JSON.parse(data.toString()));
     }
